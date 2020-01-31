21ab58bbd91d65257f1f40f4686915ab5fd2a8d3
Oleksandr Boiko <nezhraba@gmail.com>
2020-01-31 20:45:13 +0200

    useInput custom hook
    Took 9 minutes

--- src/Todo/AddTodo.js
+++ src/Todo/AddTodo.js
@@ -1,19 +1,33 @@
 import React, {useContext, useState} from "react";
 import Context from "../context";
 
-export default function () {
-    const [value, setValue] = useState('');
+function useInput(defaultValue = "") {
+    const [value, setValue] = useState(defaultValue);
 
+    return {
+        get: () => value,
+        set: (value) => setValue(value),
+        html: function () {
+            return {
+                value,
+                onChange: evt => setValue(evt.target.value),
+            };
+        }
+    };
+}
+
+export default function () {
+    const input = useInput();
     const {addTodo} = useContext(Context);
     function submit(evt) {
         evt.preventDefault();
-        addTodo(value);
-        setValue('');
+        addTodo(input.get());
+        input.set('');
     }
 
     return (
         <form onSubmit={submit}>
-            <input type="text" value={value} onChange={evt => setValue(evt.target.value)}/>
+            <input type="text" {...input.html()}/>
             <button type="submit">
                 Add
             </button>
